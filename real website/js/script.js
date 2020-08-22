d99d06b4fb35cafb0c8f88108c4ac8cd8dd18622
AsadullahGalib007 <abrgalib@gmail.com>
2020-08-22 22:38:33 +0600

    Implementing Ajax(P1)

--- real website/js/script.js
+++ real website/js/script.js
@@ -18,3 +18,36 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     $(event.target).focus();
   });
 });
+
+
+(function (global) {
+	var dc = {};
+	var homeHtml = "snippets/home-snippet.html";
+	// Convinience function for inserting innerHTML for 'select'
+	var insertHtml = function(selector, html){
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+
+	// Show loading icon inside element identified by 'selector'.
+	var showLoading = function (selector){
+		var html = "<div class = 'text-center'>";
+		html += "<img src='img/ajax-loader.gif'></div>";
+		insertHtml(selector, html);
+	};
+
+	document.addEventListener("DOMContentLoaded", function(event){
+
+		// On first load, show home view
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(
+			homeHtml,
+			function(responseText){
+				document.querySelector("#main-content")
+				.innerHTML = responseText;
+			},
+			false);
+	});
+	global.$dc = dc;
+
+})(window);
